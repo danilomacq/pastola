b92f4ad3b50cfe09f673e5d1a8fb3641cbb79277
Hmoretti <moretti.hernani@gmail.com>
2019-11-06 21:50:15 -0300

    subindo aulinha e alterar pastas aula certa

---
+++ aula-08/js/script.js
@@ -0,0 +1,9 @@
+let material = prompt("Qual é o seu material?");
+let tamanhoEscada = Number(prompt("Qual o tamanho da sua escada em números?"));
+let escada = "";
+
+// FOR 
+
+for (let index = 0; index < tamanhoEscada; index++) {
+    console.log(escada = escada + material);
+}
