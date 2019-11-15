cb2aed83c6f225c655c23f33fc905c67ef22162d
unknown <danilomacq@gmail.com>
2019-11-14 21:58:45 -0300

    Primeiro projeto Djano com index e sobre

---
+++ django-01/landing/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mostrar_index(req):
+    return render(req, "index.html")
+
+def mostrar_sobre(req):
+    return render(req, "sobre/index.html")
